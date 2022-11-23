00082cba45f06c66e22c637a399b1273a36f9bfa
Magnitoshka <lalkaioleg@gmail.com>
2022-11-23 21:52:05 +0200

    Is it working
    Is it working

---
+++ Assets/Collider2.cs
@@ -0,0 +1,3 @@
+﻿internal class Collider2
+{
+}
---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour,IDamageable
+{
+    Transform targetDestination;
+    GameObject targetGameObject;
+    Character targetCharacter;
+    [SerializeField] float speed;
+
+    Rigidbody2D rgdb2d;
+    [SerializeField] int hp = 99;
+    [SerializeField] int damage = 10;
+
+
+
+    private void Awake()
+    {
+        rgdb2d=GetComponent<Rigidbody2D>();
+        
+    }
+
+    public void SetTarget(GameObject target)
+    {
+        targetGameObject = target;
+        targetDestination = target.transform;
+    }
+
+
+    private void FixedUpdate()
+    {
+        Vector3 direction = (targetDestination.position - transform.position).normalized;
+        rgdb2d.velocity = direction * speed;
+    }
+
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject== targetGameObject)
+        {
+            Attack();
+        }
+    }
+
+    private void Attack()
+    {
+        if (targetCharacter==null)
+        {
+            targetCharacter=targetGameObject.GetComponent<Character>();
+        }
+
+        targetCharacter.TakeDamage(damage);
+    }
+    public void TakeDamage(int damage)
+    {
+        hp -= damage;
+        if (hp < 1)
+        {
+            Destroy(gameObject);
+        }
+    }
+  }
