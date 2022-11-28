2da64a06c79b2d91cb2a8e3855144543645c2f1a
Magnitoshka <lalkaioleg@gmail.com>
2022-11-28 04:56:33 +0200

    Spawning objects and Disposing unused objects
    оптимизация путём удаления далёких от игрока объектов

--- Assets/Scripts/DropOnDestroy.cs
+++ Assets/Scripts/DropOnDestroy.cs
@@ -5,11 +5,25 @@ using UnityEngine;
 public class DropOnDestroy : MonoBehaviour
 {
     [SerializeField] GameObject dropItemPrefab;
-    [SerializeField][Range(0f,1f)] float chance = 1f;    
+    [SerializeField][Range(0f,1f)] float chance = 1f;
 
+    bool isQuitting = false;
+
+    private void OnApplicationQuit()
+    {
+        isQuitting = true;
+    }
 
     private void OnDestroy()
     {
+        if (isQuitting)
+        {
+            return;
+        }
+
+        {
+
+        }
         if(Random.value < chance)
         {
             Transform t = Instantiate(dropItemPrefab).transform;
